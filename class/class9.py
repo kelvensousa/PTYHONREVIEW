1fe0adcefce730403bffdc931b64ee0cac79ea60
KELVEN ALVES DE SOUSA <93238088+kelvensousa@users.noreply.github.com>
2022-07-18 18:40:14 -0300

    Update class9.py

---
+++ class/class9.py
@@ -0,0 +1,25 @@
+# This example show us the string slicing
+
+pharse = "Welcome to back this code"
+print(pharse[:5])
+print(pharse[9::3])
+print(pharse[1:15])
+print(pharse[1:15:3])
+
+# len(phrase) is a function.
+# pharse.count('o') will show me how many letters it has in the sentence.
+# pharse.find('ode') how many times show me ode.
+# pharse.replace('back','stronger') replace one word with another.
+# pharse.upper() convert lowercase to uppercase
+# pharse.upper() convert uppercase  to lowercase
+# pahrse.capitalize() Make the first letter uppercase and the rest lowercase.
+# pharse.title()
+# pharse.rstrip()
+# pharse.lstrip()
+
+# Welcome to back this code in phrase
+
+# This example of string division.
+# phrase.split()
+
+# This example of string junction
